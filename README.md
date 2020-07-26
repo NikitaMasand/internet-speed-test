c4f029e7b8520c916ed78e60a32ea041b4009939
NikitaMasand <masandnikita@gmail.com>
2020-07-27 00:44:04 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# internet-speed-test"# internet-speed-test" 
---
+++ src/main/java/com/internet/speed/internet/speed/worker/CSVWorker.java
@@ -0,0 +1,37 @@
+package com.internet.speed.internet.speed.worker;
+
+import com.internet.speed.internet.speed.model.CSVObject;
+import com.opencsv.CSVWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class CSVWorker implements Runnable {
+    Logger logger = LoggerFactory.getLogger(CSVWorker.class);
+
+    CSVObject csvObject;
+
+    public CSVWorker(CSVObject csvObject) {
+        this.csvObject = csvObject;
+    }
+
+    @Override
+    public void run() {
+        logger.info("Writing to csv file starts");
+        String csvPath = "./speed-test-data.csv";
+        File file = new File(csvPath);
+        try {
+            FileWriter outputCSVFile = new FileWriter(file, true);
+            CSVWriter csvWriter = new CSVWriter(outputCSVFile);
+            String[] csvObjectData = {csvObject.getCurrentDateTime(), String.valueOf(csvObject.getSpeed())};
+            csvWriter.writeNext(csvObjectData);
+            csvWriter.close();
+            logger.info("Writing to file completed");
+        } catch (Exception e) {
+            logger.error("Error while writing to file {}", e.getMessage());
+        }
+
+    }
+}
